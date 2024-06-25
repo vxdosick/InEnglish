f9bd6aa5ebd95b4c77a70716149a3430abb50538
Vadym Foteniuk <vadumfotenuk@gmail.com>
2024-06-25 04:46:33 +0300

    Debuging VUE_API_URL

--- client/src/apiClient.js
+++ client/src/apiClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: process.env.VUE_APP_API_BASE_URL,
+  baseURL: process.env.production.VUE_APP_API_BASE_URL,
   headers: {
     'Content-Type': 'application/json'
   }
